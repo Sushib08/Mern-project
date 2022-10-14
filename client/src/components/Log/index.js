3c5ec0382b4d5e6378f29314432e2155040e3bab
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-10-14 19:25:30 +0200

    SignInForm completed

--- client/src/components/Log/index.js
+++ client/src/components/Log/index.js
@@ -1,18 +1,19 @@
+
 import React, { useState } from "react";
 import SignInForm from "./SignInForm";
 import SignUpForm from "./SignUpForm";
 
-function Log(props) {
-  const [SignUpModal, setSignUpModal] = useState(props.signup);
-  const [SignInModal, setSignInModal] = useState(props.signin);
+const Log = ( props ) => {
+  const [signUpModal, setSignUpModal] = useState(props.signup);
+  const [signInModal, setSignInModal] = useState(props.signin);
 
   const handleModals = (e) => {
     if (e.target.id === "register") {
       setSignInModal(false);
       setSignUpModal(true);
     } else if (e.target.id === "login") {
-      setSignInModal(true);
       setSignUpModal(false);
+      setSignInModal(true);
     }
   };
 
@@ -23,23 +24,23 @@ function Log(props) {
           <li
             onClick={handleModals}
             id="register"
-            className={SignUpModal ? "active-btn" : null}
+            className={signUpModal ? "active-btn" : null}
           >
             S'inscrire
           </li>
           <li
             onClick={handleModals}
             id="login"
-            className={SignInModal ? "active-btn" : null}
+            className={signInModal ? "active-btn" : null}
           >
             Se connecter
           </li>
         </ul>
-        {SignUpModal && <SignUpForm />}
-        {SignInModal && <SignInForm />}
+        {signUpModal && <SignUpForm />}
+        {signInModal && <SignInForm />}
       </div>
     </div>
   );
-}
+};
 
 export default Log;
--- server.js
+++ server.js
@@ -11,15 +11,15 @@ const cors = require('cors')
 const app= express();
 
 const corsOptions = {
-    origin : process.env.CLIENT_URL,
-    credentials: true,
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
+    origin: process.env.CLIENT_URL,
+  credentials: true,
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
 }
 
-app.use(cors({corsOptions}));
+app.use(cors(corsOptions))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
