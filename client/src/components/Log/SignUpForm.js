25d06307b54684c4bdde4a8ff4fa050e5690c076
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-10-14 13:44:12 +0200

    create SignUpform

--- client/src/components/Log/SignUpForm.js
+++ client/src/components/Log/SignUpForm.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignUpForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {};
+
   return (
-    <div> Inscription
-    </div>
+    <form action="" onSubmit={handleLogin} id="sign-up-form">
+      <label htmlFor="email">Email</label>
+      <br />
+      <input
+        type="text"
+        name="email"
+        id="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <br />
+      <label htmlFor="password">Mot de passe</label>
+      <br/>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <input type="submit" value="Se connecter" />
+            <br/>
+    </form>
   );
 }
 
 export default SignUpForm;
+//1:01
