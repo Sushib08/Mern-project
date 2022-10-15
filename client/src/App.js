e981d86244d29ee7ba44058171c391029ce15205
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-10-15 10:17:09 +0200

    SignUpForm completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
+import axios from "axios";
 
 export default function App() {
-  return <Routes />;
+  const [uid, setUid] = useState(null);
+
+  useEffect(() =>{
+    const fetchToken = async() =>{
+      
+    await axios ({
+      method : "get",
+      url : `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials : true,
+    })
+    .then((res) => setUid(res.data))
+    .catch((err) => console.log("No token"));
+  }
+  fetchToken();
+  }, [uid]); // On met des crochets pour qu'il ne s'exécute pas à l'infini
+
+  return (
+    <UidContext.Provider value={uid}>
+      <Routes />
+    </UidContext.Provider>
+  );
 }
