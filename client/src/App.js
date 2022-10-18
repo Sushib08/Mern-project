7371e122f3a974dd90c7b5a4d1d4f3193de72919
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-10-18 19:35:19 +0200

    redux intro completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,43 @@ import React, { useEffect, useState } from "react";
 import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
 import axios from "axios";
+import { useDispatch } from 'react-redux'
+import { getUser } from "./actions/user.actions";
 
 export default function App() {
   const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(async() =>{
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => setUid(res.data))
+        .catch((err) => console.log("No token"));
+    });
 
-  useEffect(() =>{
-    const fetchToken = async() =>{
-      
-    await axios ({
-      method : "get",
-      url : `${process.env.REACT_APP_API_URL}jwtid`,
-      withCredentials : true,
-    })
-    .then((res) => setUid(res.data))
-    .catch((err) => console.log("No token"));
-  }
-  fetchToken();
-  }, [uid]); // On met des crochets pour qu'il ne s'exécute pas à l'infini
+    if (uid) dispatch(getUser(uid));
+    // Safe to add dispatch to the dependencies array
+  }, [uid, dispatch])
+
+  
+  // useEffect(() => {
+  //   const fetchToken = async () => {
+  //     await axios({
+  //       method: "get",
+  //       url: `${process.env.REACT_APP_API_URL}jwtid`,
+  //       withCredentials: true,
+  //     })
+  //       .then((res) => setUid(res.data))
+  //       .catch((err) => console.log("No token"));
+  //   };
+  //   fetchToken();
+
+  //   if (uid) dispatch(getUser(uid));
+  // }, [uid]); // On met des crochets pour qu'il ne s'exécute pas à l'infini
 
   return (
     <UidContext.Provider value={uid}>
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+//import logger from "redux-logger" // pour afficher dans la console
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
