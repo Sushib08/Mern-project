fd5003c55a04847a8c2dfaa9c788c71639dbcf54
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-10-23 19:05:18 +0200

    updateProfil completed

--- client/src/reducers/user.reducer.js
+++ client/src/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import { GET_USER, UPLOAD_PICTURE } from "../actions/user.actions";
+import { GET_USER, UPDATE_BIO, UPLOAD_PICTURE } from "../actions/user.actions";
 
 const initialState = {};
 
@@ -7,12 +7,16 @@ export default function userReducer(state = initialState, action) {
     case GET_USER:
       return action.payload;
     case UPLOAD_PICTURE:
-        return {
-            ...state,
-            picture: action.payload,
-          };
+      return {
+        ...state,
+        picture: action.payload,
+      };
+    case UPDATE_BIO:
+      return {
+        ...state,
+        bio: action.payload,
+      };
     default:
       return state;
   }
 }
-//3:15:52
